7d8e446f2ecda47a365a1d2667ee11e8161277db
aeschi2305 <dennis.aeschbacher@students.fhnw.ch>
2020-04-13 16:54:00 +0200

    added NIOS implementation

---
+++ quartus/NIOS/NIOSSystem/NIOSSystem_inst.vhd
@@ -0,0 +1,13 @@
+	component NIOSSystem is
+		port (
+			clk_clk       : in std_logic := 'X'; -- clk
+			reset_reset_n : in std_logic := 'X'  -- reset_n
+		);
+	end component NIOSSystem;
+
+	u0 : component NIOSSystem
+		port map (
+			clk_clk       => CONNECTED_TO_clk_clk,       --   clk.clk
+			reset_reset_n => CONNECTED_TO_reset_reset_n  -- reset.reset_n
+		);
+
